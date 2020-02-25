67202882c5ac3cf1605080c6fe0436b208bb7b2b
fitriruhimah <fitriruhimah123@gmail.com>
2020-02-25 15:18:29 +0700

    Livecode Bootcamp

--- test_bootcamp.rb
+++ test_bootcamp.rb
@@ -27,16 +27,49 @@ puts
 puts "======================================================="
 
 # Nomer 3
-buah = {apel: 5000, nanas: 10000, semangka: 20000, jeruk: 4000, anggur: 6000}
-apel = buah[:apel]
-semangka = buah[:semangka]
-harga1 = apel * 4
-harga2 = semangka * 2
-harga3 = harga1 + harga2
-if harga3 > 50000
-    diskon = harga3 * 10/100
-    total = harga3 - diskon
+buah = {apel: 5000, nanas: 10000, semangka: 20000, jeruk:4000, anggur: 6000}
+
+print "Jumlah Apel : "
+jumlahApel = gets.to_i
+print "Jumlah Nanas : "
+jumlahNanas = gets.to_i
+print "Jumlah Semangka : "
+jumlahSemangka = gets.to_i
+print "Jumlah Jeruk : "
+jumlahJeruk = gets.to_i
+print "Jumlah Anggur : "
+jumlahAnggur = gets.to_i
+
+apel = buah[:apel] * jumlahApel
+nanas = buah[:nanas] * jumlahNanas
+semangka = buah[:semangka] * jumlahSemangka
+jeruk = buah[:jeruk] * jumlahJeruk
+anggur = buah[:anggur] * jumlahAnggur
+
+harga = apel + nanas + semangka + jeruk + anggur
+
+if harga > 50000
+    diskon = harga * 10/100
+    total = harga - diskon
 else
-    total
+    total = harga
 end
+
+puts "=== Buah Yang Dibeli ==="
+if jumlahApel > 0
+    puts "#{jumlahApel} Apel = #{apel}"
+end
+if jumlahNanas > 0
+    puts "#{jumlahNanas} Nanas = #{nanas}"
+end
+if jumlahSemangka > 0
+    puts "#{jumlahSemangka} Semangka = #{semangka}"
+end
+if jumlahJeruk > 0
+    puts "#{jumlahJeruk} Jeruk = #{jeruk}"
+end
+if jumlahAnggur > 0
+    puts "#{jumlahAnggur} Anggur = #{anggur}"
+end
+puts "=== Total Harga ==="
 puts total
